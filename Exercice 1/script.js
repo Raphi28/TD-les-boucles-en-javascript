51371391380cacf75afd3ecf14f4ee4975b62646
Raphi28 <143432466+Raphi28@users.noreply.github.com>
2024-11-07 20:10:28 +0100

    Exercice 1

---
+++ Exercice 1/script.js
@@ -0,0 +1,31 @@
+for (let i = 0; i < 11; i++){
+    console.log(i);
+}
+
+for (let i = 10; i >= 0; i--){
+    console.log(i);
+}
+
+let compteur = 0
+while (compteur < 11) {
+    console.log(compteur);
+    compteur ++;
+}
+
+let compteur2 = 10
+while (compteur2 >= 0) {
+    console.log(compteur2);
+    compteur2 --;
+}
+
+let i = 0;
+do {
+    console.log(i);
+    i ++;
+} while (i < 11);
+
+let i2 = 10;
+do {
+    console.log(i2);
+    i2 --;
+} while (i2>=0);
