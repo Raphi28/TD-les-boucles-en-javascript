dc9b4e64f53be1c7fd79b76f2a89e64bcf6e3803
Raphi28 <143432466+Raphi28@users.noreply.github.com>
2024-11-07 20:12:23 +0100

    Exercice 7

---
+++ Exercice 7/script.js
@@ -0,0 +1,10 @@
+let taille = prompt("Quel doit être la taille de l'échiquier")
+
+let ligne1 = ""
+let ligne2 = ""
+
+while (taille){
+    ligne1 += "#"
+    ligne2 += " "
+    console.log(ligne1 + ligne2);
+}
